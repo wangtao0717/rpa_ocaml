31c28ac4efbaa6344bbb8be131661d18e6d2bc8d
Wang Tao <77219816+wangtao0717@users.noreply.github.com>
2021-03-17 22:03:58 +0800

    Update rpa_ocaml.ml
    use type taget and taget_range to decrease function name.

--- rpa_ocaml.ml
+++ rpa_ocaml.ml
@@ -7,12 +7,19 @@ struct
   open Printf
   open Base
 
-  let() = Py.initialize ();;
+  let () = Py.initialize ();;
 
   type taget =
     | Coordinate of int * int
     | Element of string
   ;;  
+
+  type taget_range =
+    | Range of int * int * int* int
+    | Element_range of string
+  ;; 
+
+  exception Invalid_input;;
 
   let bool_to_UpString bool = 
     match bool with 
@@ -20,7 +27,7 @@ struct
     |false -> "False"
   ;;
 
-let r = Py.import "rpa";;
+  let r = Py.import "rpa";;
 
 (*core fuction *)
 (*start TagUI, auto-setup on first run *)
@@ -41,119 +48,96 @@ let r = Py.import "rpa";;
 
 (*basic funtion *)
 (**go to web URL, *)
-let url webUrl = 
-  let web = Py.String.of_string webUrl in
-    Py.Module.get_function r "url" [|web|];
-    ();;
+  let url webUrl = 
+    let web = Py.String.of_string webUrl in
+      Py.Module.get_function r "url" [|web|];
+      ()
+  ;;
 
 (* get current URL*)      
-let getUrl ()=
-  let url = Py.Module.get_function r "url" [||] in
-    Py.String.to_string url
-;;
+  let getUrl ()=
+    let url = Py.Module.get_function r "url" [||] in
+      Py.String.to_string url
+  ;;
 
-(**left click a element if find it by the keyword *)
-let click_element element  = 
-  let element = Py.String.of_string element in
-    Py.Module.get_function r "click" [|element|];
-    ();;
-
-(**left click a element by coordor coordinateinate*)
-let click_coordinate x y  = 
-  let coordinate =  Array.map [|x;y|] Py.Int.of_int in
-    Py.Module.get_function r "click" coordinate;
-    ();;
+(**left click a element  if find it by the keyword or coordinate*)
+  let click element_or_coordinate = 
+    match element_or_coordinate with 
+    |Element element -> let element = Py.String.of_string element in
+                  Py.Module.get_function r "click" [|element|];();
+    |Coordinate(x,y) -> let coordinate =  Array.map [|x;y|] Py.Int.of_int in
+                  Py.Module.get_function r "click" coordinate;();
+  ;;
 
 
-(**right click a element if find it by the keyword *)
-let rclick_element element  = 
-  let element = Py.String.of_string element in
-    Py.Module.get_function r "rclick" [|element|];
-    ();;
+(**right click a element if find it by the keyword or coordinate*)
+  let rclick element_or_coordinate = 
+    match element_or_coordinate with 
+    |Element element -> let element = Py.String.of_string element in
+                  Py.Module.get_function r "rclick" [|element|];();
+    |Coordinate(x,y) -> let coordinate =  Array.map [|x;y|] Py.Int.of_int in
+                  Py.Module.get_function r "rclick" coordinate;();
+  ;;
 
-(**right click a element by coordinate*)
-let rclick_coordinate x y  = 
-  let coordinate =  Array.map [|x;y|] Py.Int.of_int in
-    Py.Module.get_function r "rclick" coordinate;
-    ();;
+(**double click a element if find it by the keyword or coordinate*)
+  let dclick element_or_coordinate = 
+    match element_or_coordinate with 
+    |Element element -> let element = Py.String.of_string element in
+                  Py.Module.get_function r "dclick" [|element|];();
+    |Coordinate(x,y) -> let coordinate =  Array.map [|x;y|] Py.Int.of_int in
+                  Py.Module.get_function r "dclick" coordinate;();
+  ;;
 
-(**double click a element if find it by the keyword *)
-let dclick_element element  = 
-  let element = Py.String.of_string element in
-    Py.Module.get_function r "dclick" [|element|];
-    ();;
+(**move mouse to a element if find it by the keyword or coordinate*)
+  let hover element_or_coordinate = 
+    match element_or_coordinate with 
+    |Element element -> let element = Py.String.of_string element in
+                  Py.Module.get_function r "hover" [|element|];();
+    |Coordinate(x,y) -> let coordinate =  Array.map [|x;y|] Py.Int.of_int in
+                  Py.Module.get_function r "hover" coordinate;();
+  ;;
 
-(**double click a element by coordinate*)
-let dclick_coordinate x y  = 
-  let coordinate =  Array.map [|x;y|] Py.Int.of_int in
-    Py.Module.get_function r "dclick" coordinate;
-    ();;
-
-(**move mouse to a element if find it by the keyword *)
-let hover_element element  = 
-  let element = Py.String.of_string element in
-    Py.Module.get_function r "hover" [|element|];
-    ();;
-
-(**move mouse to a element by coordinate*)
-let hover_coordinate x y  = 
-  let coordinate =  Array.map [|x;y|] Py.Int.of_int in
-    Py.Module.get_function r "hover" coordinate;
-    ();;
-
-(**enter text at element found by the keyword *)
-let type_element element text = 
-  let element = Py.String.of_string element in
-  let text = Py.String.of_string text in
-    Py.Module.get_function r "type" [|element;text|];
-    ();;
-
-(**enter text at element found by coordinate *)
-let type_coordinate x y text = 
-  let x = Py.Int.of_int x in
-  let y = Py.Int.of_int y in
-  let text = Py.String.of_string text in
-    Py.Module.get_function r "type"
-     [|x;y;text|];
-    ();;
+(**	enter text at element if find it by the keyword or coordinate*)
+  let type_text element_or_coordinate text= 
+    let text = Py.String.of_string text in
+    match element_or_coordinate with 
+    |Element element -> let element = Py.String.of_string element in
+                  Py.Module.get_function r "type" [|element;text|];();
+    |Coordinate(x,y) -> let x = Py.Int.of_int x in
+                        let y = Py.Int.of_int y in
+                  Py.Module.get_function r "type" [|x;y;text|];();
+  ;;
 
 (*choose dropdown option*)
- let select_elem_option element option_value =
-    let element = Py.String.of_string element in
-    let option_value = Py.String.of_string option_value in
-    Py.Module.get_function r "select" [|element;option_value|];
-    ();;
-  
-  let select_elemXY_option elementX elementY option_value =
-    let elementX = Py.Int.of_int elementX in
-    let elementY = Py.Int.of_int elementY in
-    let option_value = Py.String.of_string option_value in
-    Py.Module.get_function r "select" [|elementX;elementY;option_value|];
-    ();;
 
-  let select_elem_optionXY element option_valueX option_valueY =
-    let option_valueX = Py.Int.of_int option_valueX in
-    let option_valueY = Py.Int.of_int option_valueY in
-    let element = Py.String.of_string element in
-    Py.Module.get_function r "select" [|element;option_valueX;option_valueY|];
-    ();;
-  
-  let select_elemXY_optionXY elementX elementY option_valueX option_valueY =
-    let input = Array.map [|elementX elementY option_valueX option_valueY|] Py.Int.of_int in
-    Py.Module.get_function r "select" input;
-    ();;
+  let select element option_value =
+    match element with 
+    |Element element -> let element = Py.String.of_string element in
+        match option_value with 
+        |Element option_value -> let option_value = Py.String.of_string option_value in
+                                Py.Module.get_function r "select" [|element;option_value|];();
+        |Coordinate (x_option,y_option) -> let x_option = Py.Int.of_int x_option in
+                                          let y_option = Py.Int.of_int y_option in
+                                          Py.Module.get_function r "select" [|element;x_option;y_option|];();                                
+    |Coordinate(x_element,y_element) -> let x_element = Py.Int.of_int x_element in
+                                        let y_element = Py.Int.of_int y_element in
+        match option_value with 
+        |Element option_value -> let option_value = Py.String.of_string option_value in
+                                Py.Module.get_function r "select" [|x_element;y_element;option_value|];();
+        |Coordinate (x_option,y_option) -> let x_option = Py.Int.of_int x_option in
+                                          let y_option = Py.Int.of_int y_option in
+                                          Py.Module.get_function r "select" [|x_element;y_element;x_option;y_option|];();  
+  ;;                      
 
   (**fetch & return element text *)
-  let read_element element = 
-    let element = Py.String.of_string element in 
-    let result = Py.Module.get_function r "read" [|element|] in
-    Py.String.to_string result
-  ;;
-  
-  let read_coordinate x1 y1 x2 y2 = 
-    let input = Array.map [|x1;y1;x2;y2|] Py.Int.of_int in
-    let result = Py.Module.get_function r "read" input in
-    Py.String.to_string result
+  let read range_of_element = 
+    match range_of_element with 
+    |Element_range element -> let element = Py.String.of_string element in
+                  let result = Py.Module.get_function r "read" [|element|] in
+                  Py.String.to_string result
+    |Range(x1,y1,x2,y2) -> let input = Array.map [|x1;y1;x2;y2|] Py.Int.of_int in
+                  let result = Py.Module.get_function r "read" input in
+                  Py.String.to_string result
   ;;
 
   (**save screenshot to file *)
@@ -191,7 +175,7 @@ let type_coordinate x y text =
     let input = [|Py.String.of_string text_to_prompt|] in
     let result = Py.Module.get_function r "ask" input in
     Py.String.to_string result
-    ;;
+  ;;
 
 
 (*PRO FUNCTIONS *)
@@ -200,7 +184,7 @@ let keyboard keys_and_modifiers =
   let keys_and_modifiers = Py.String.of_string keys_and_modifiers in
   Py.Module.get_function r "keyboard" [|keys_and_modifiers|];
   ();;
-exception Invalid_input;;
+
 (**send mouse event to screen 'down' or 'up' *)
 let mouse move = 
   let move = match (String.lowercase move) with
@@ -211,7 +195,7 @@ let mouse move =
     ();;
 
 (**explicitly wait for some time, default 5 seconds*)
-let wait ?(time=5.0)  =
+let wait ?(time=5.0) () =
   let time =  Py.Float.of_float time in
    Py.Module.get_function r "wait" [|time|];
    ();;
@@ -227,8 +211,9 @@ let upload element_CSS filename =
   let input = Array.map [|element_CSS;filename|] Py.String.of_string in
   Py.Module.get_function r "upload" input;
   ();;
+
 (**download from URL to file *)
-let download url ?filename = 
+let download url ?filename () = 
   let filename = match filename with 
   |None ->""
   |Some x -> x in
@@ -237,13 +222,13 @@ let download url ?filename =
   ();;
 
 (**unzip zip file to specified location *)
-let unzip file_to_unzip ?unzip_location =
-let unzip_location = match unzip_location with 
-  |None ->""
-  |Some x -> x in
-  let input = Array.map [|file_to_unzip;unzip_location|] Py.String.of_string in
-    Py.Module.get_function r "unzip" input;
-    ();;
+let unzip file_to_unzip ?unzip_location () =
+  let unzip_location = match unzip_location with 
+    |None ->""
+    |Some x -> x in
+    let input = Array.map [|file_to_unzip;unzip_location|] Py.String.of_string in
+      Py.Module.get_function r "unzip" input;
+      ();;
 
 (**set web frame, frame() to reset *)
 let frame ?(main_frame = "") ?(subframe = "") ()=
@@ -252,7 +237,7 @@ let frame ?(main_frame = "") ?(subframe = "") ()=
       ();;
 
 (**set context to web popup tab ,no parameter to reset to main page*)
-let popup ?(string_in_url="") =
+let popup ?(string_in_url="") () =
   let string_in_url = Py.String.of_string string_in_url in
   Py.Module.get_function r "popup" [|string_in_url|];
   ();;
@@ -262,14 +247,14 @@ let run command =
   let command = Py.String.of_string command in
   let output = Py.Module.get_function r "run" [|command|] in
   Py.String.to_string output
-  ;;
+;;
 
 (**run code in DOM & return output *)
 let dom statement_to_run =
   let statement_to_run = Py.String.of_string statement_to_run in
   let output = Py.Module.get_function r "dom" [|statement_to_run|] in
   Py.String.to_string output
-  ;;
+;;
 
 (**run custom SikuliX commands *)
 let vision command =
@@ -287,7 +272,7 @@ let timeout time =
 let get_timeout () =
   let timeout = Py.Module.get_function r "timeout" [||] in
   Py.Float.to_float timeout
-  ;;
+;;
 
 
 (*HELPER FUNCTIONS *)
@@ -296,27 +281,28 @@ let exist element =
   let element = Py.String.of_string element in
   let is_exist = Py.Module.get_function r "exist" [|element|] in
   Pycaml.py_is_true is_exist
-  ;;
+;;
 
 (**return true or false if element is present now *)
 let present element = 
   let element = Py.String.of_string element in
   let is_present = Py.Module.get_function r "present" [|element|] in
   Pycaml.py_is_true is_present
-  ;;
+;;
 
 (**return number of web elements  *)
 let count element = 
   let element = Py.String.of_string element in
     let count = Py.Module.get_function r "count" [|element|] in
     Py.Int.to_int count
-    ;;
+;;
 
 (**put text to clipboard*)
 let clipboard_put_text text = 
   let text = Py.String.of_string text in 
   Py.Module.get_function r "clipboard" [|text|];
   ();;
+
 (**return clipboard text as string *)
 let clipboard_return_text () =
   let text = Py.Module.get_function r "clipboard" [||] in
@@ -333,31 +319,31 @@ let mouse_xy () =
 let mouse_x () =
   let result = Py.Module.get_function r "mouse_x" [||] in
   Py.Int.to_int result
-  ;;
+;;
 
 (**return y coordinate of mouse *)
 let mouse_y () =
   let result = Py.Module.get_function r "mouse_y" [||] in
   Py.Int.to_int result
-  ;;
+;;
 
 (**return page title of current web page as string *)
 let title () =
   let result = Py.Module.get_function r "title" [||] in
   Py.String.to_string result
-  ;;
+;;
 
 (**return text content of current web page as string *)
 let text () =
   let result = Py.Module.get_function r "text" [||] in
   Py.String.to_string result
-  ;;
+;;
 
 (**return time elapsed in sec between calls as float *)
 let timer () =
   let result = Py.Module.get_function r "timer" [||] in
   Py.Float.to_float result
-  ;;
+;;
 
 (*file  *)
 let write_to_file file message =
@@ -375,13 +361,6 @@ let writeArray_to_file file messageArray =
     close_out oc; 
   ;;
 
-let read_whole_file filename =
-  let ic = open_in_bin filename in
-    let s = really_input_string ic ((in_channel_length ic)-1) in
-      close_in ic;
-      s;
-  ;;
-
 let addArray_to_file file messageArray = 
   let beforemessage = read_whole_file file in
     let oc = open_out file in
@@ -391,8 +370,14 @@ let addArray_to_file file messageArray =
       done;
       fprintf oc "\n";  
       close_out oc; 
-  ;;
+;;
 
+let read_whole_file filename =
+  let ic = open_in_bin filename in
+    let s = really_input_string ic ((in_channel_length ic)-1) in
+      close_in ic;
+      s;
+;;
 
 let writeArray_to_csv csvfile messageArray =
   let oc = open_out csvfile in 
@@ -403,7 +388,7 @@ let writeArray_to_csv csvfile messageArray =
     done;  
     fprintf oc "\n";
     close_out oc; 
-  ;;
+;;
 
 let addArray_to_csv csvfile messageArray = 
   let beforemessage = read_whole_file csvfile in
@@ -416,7 +401,7 @@ let addArray_to_csv csvfile messageArray =
       done;
       fprintf oc "\n";  
       close_out oc; 
-    ;;
+;;
 
 let readline_from_file  file =
   let ic = open_in file in
@@ -428,7 +413,7 @@ let readline_from_file  file =
     with e ->                      
       close_in_noerr ic;          
       raise e      
-    ;;
+;;
   
 
 let rec read_whole_filelist filelist =
@@ -437,5 +422,6 @@ let rec read_whole_filelist filelist =
     |x::tail
       ->let ic = open_in_bin x in
       (really_input_string ic (in_channel_length ic)) ::(read_whole_filelist tail)
-      ;;
+;;
+
 end;;
